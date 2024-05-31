681de4644ef7dbc4f422c60b9cd4741c880bbedb
Ivan Goncharov <givan1010@gmail.com>
2024-05-31 23:33:12 +0500

    validations

--- server/router/index.js
+++ server/router/index.js
@@ -2,7 +2,13 @@ const Router = require('express').Router;
 const userController = require('../controllers/user-controller')
 const router = new Router();
 
-router.post('/registration', userController.registration)
+const {body} = require('express-validator')
+
+
+router.post('/registration',
+    body('email').isEmail(),
+    body('password').isLength({min: 3, max: 32}),
+    userController.registration)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.get('/activate/:link', userController.activate)
